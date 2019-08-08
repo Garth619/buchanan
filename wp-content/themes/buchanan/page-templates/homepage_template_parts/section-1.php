13296eaf66db9b7a9a26b1114b4f27f04526291a
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-07 18:14:23 -0700

    push

--- wp-content/themes/buchanan/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/buchanan/page-templates/homepage_template_parts/section-1.php
@@ -1,6 +1,30 @@
 <section id="section_one">
-	
 		
+		<div class="section_one_inner">
+			
+			<span class="sec_one_subheader">Injury Attorney</span><!-- sec_one_subheader -->
+			
+			<span class="sec_one_large_header">Righting Wrongs</span><!-- sec_one_large_header -->
+			
+			<ul class="sec_one_sp_list">
+				
+				<li>No Fee Unless You Win</li>
+				<li>Dedicated Legal Guide</li>
+				<li>Medical Experts On Team</li>
+				
+			</ul><!-- sec_one_sp_list -->
+			
+		</div><!-- section_one_inner -->
+		
+		<div class="parallax">
+			
+			<img id="para_one" src="<?php bloginfo('template_directory');?>/images/layer-3.jpg"/>
+			
+			<img id="para_two" src="<?php bloginfo('template_directory');?>/images/layer-2.png"/>
+			
+			<img id="para_three" src="<?php bloginfo('template_directory');?>/images/layer-1.png"/>
+			
+		</div><!-- updated_parallax -->
 
 	
 </section><!-- section_one -->
