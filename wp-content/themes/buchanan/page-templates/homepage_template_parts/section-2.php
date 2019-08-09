5fd6efdb7fba1a9df163df945989d20992e63cac
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-09 13:19:51 -0700

    push

--- wp-content/themes/buchanan/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/buchanan/page-templates/homepage_template_parts/section-2.php
@@ -1,12 +1,55 @@
 <section id="section_two">
 	
+	<img class="circles" src="<?php bloginfo('template_directory');?>/images/graphic-dots.svg"/>
+	
 	<div class="sec_two_top">
 		
 		<h1><span class="large_header">Michigan</span> <span class="sub_header">Personal Injury Attorneys</span></h1>
 		
 		<span class="sec_two_top_description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 sed do esto eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis es ipsum suspendisse ultes.</span><!-- sec_two_top_description -->
-		
+
+				
 	</div><!-- sec_two_top -->
+	
+	<div class="sec_two_bottom">
+		
+		<div class="sec_two_left">
+			
+			<img class="pic" src="<?php bloginfo('template_directory');?>/images/content-img.jpg"/>
+			
+			<div class="sec_two_cta">
+			
+				<img class="sec_two_cta_img" src="<?php bloginfo('template_directory');?>/images/graphic-pullquote-dots.jpg"/>
+				
+				<span class="sec_two_cta_description">Our clients always meet with the team that handles the claim. We never pass you down to an inexperienced attorney or send you off to another firm.</span><!-- sec_two_cta_description -->
+				
+				<a class="sch_free_consult" href="#consultation">Schedule a Free Consultaton</a><!-- sch_free_consult -->
+			
+			</div><!-- sec_two_cta -->
+
+			
+		</div><!-- sec_two_left -->
+		
+		<div class="sec_two_right content">
+			
+			<h2>Lorem ipsum dolor sit amet esse dolore pariatur dolore esse sed ut derp voliptate velit esee</h2>
+			
+			<p>Duis aute irure dolor in <a href="">reprehenderit in voluptate</a> velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sinmmodo viverra maecenas estosee accumsan lacus ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  wenia veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commos commodo consequat.</p>
+
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde</p>
+			
+			<p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, Sed quia consequuntur magni dolores eos qui ratione voluptatem sequialumes nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptate.</p>
+
+
+			<h3>H3 Lorem ipsum dolor sit amet consectetur</h3>
+			
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolorem que laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicaboeaque ipsa quae abeaque ipsa quae abeaque ipsa quae abeaque ipsa quae ab. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+			
+		</div><!-- sec_two_right -->
+		
+	</div><!-- sec_two_bottom -->
 	
 </section><!-- section_two -->
