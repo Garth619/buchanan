8aec13627b9b6232db49a902f36fb3f3449b044c
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-09 18:09:26 -0700

    push

---
+++ wp-content/themes/buchanan/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,11 @@
+<section id="section_four">
+	
+	<img class="circles" src="<?php bloginfo('template_directory');?>/images/graphic-dots.svg"/>
+	
+	<div class="sec_four_top">
+		
+		<h2><span class="large_header">Experienced</span><span class="sub_header">Legal Staff To Protect And Enforce Your Legal Rights</span></h2>
+		
+	</div><!-- sec_four_top -->
+	
+</section><!-- section_four -->
