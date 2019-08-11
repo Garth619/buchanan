698cd2baa35d70693902fe58da3d5c262f9d4b9d
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-11 14:46:32 -0700

    push

--- wp-content/themes/buchanan/footer.php
+++ wp-content/themes/buchanan/footer.php
@@ -10,9 +10,29 @@
 		
 	</div><!-- form_wrapper -->
 	
+	<div class="footer_info">
+		
+		<div class="footer_info_left">
+			
+			<span class="footer_info_title">Our Office</span><!-- footer_info_title -->
+			
+			<span class="address large_text">171 Monroe Ave. N.W. Suite 750<br/> Grand Rapids, MI 49503</span><!-- address -->
+			
+			<a class="get_directions footer_link" href="">
+				
+				<span>Get Directions</span>
+				
+				<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+			
+			</a><!-- get_directions -->
+			
+		</div><!-- footer_info_left -->
+		
+		<div class="footer_info_right"></div><!-- footer_info_right -->
+		
+	</div><!-- footer_info -->
 	
-	
-	<img class="bridge desktop"src="<?php bloginfo('template_directory');?>/images/form-img-desk.jpg"/><!-- bridge -->
+	<img class="bridge desktop"src="<?php bloginfo('template_directory');?>/images/bridge-desktop.jpg"/><!-- bridge -->
 		
 </footer>
 
