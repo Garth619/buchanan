2bb898127ac712c3116b3c5b34710c34535aaf9c
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-15 16:31:24 -0700

    push

--- wp-content/themes/buchanan/page-templates/template-padirectory.php
+++ wp-content/themes/buchanan/page-templates/template-padirectory.php
@@ -5,8 +5,20 @@
 get_header(); ?>
 
 
+<div id="internal_main">
+
+	<div class="internal_wrapper">
 		
-		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
+		
+		<div class="pa_directory">
+		
+			<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'pa_menu' ) ); ?>
+		
+		</div><!-- pa_directory -->
+		
+	</div><!-- internal_wrapper -->
 
-
+</div><!-- internal_main -->	
+		
 <?php get_footer(); ?>
--- wp-content/themes/buchanan/page.php
+++ wp-content/themes/buchanan/page.php
@@ -8,41 +8,10 @@
 		
 		<div class="inner_content content">
 			
-			<h1>h1 is dolor in velit esse cilium esse incididunt es</h1>
+			<h1><?php the_title();?></h1>
 			
-			<h2>H2 rem ipsum dolor sit amet esse dolore pariatur dolore esse sed ut derp voliptate velit esee</h2>
-			
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sinmmodo viverra maecenas estosee accumsan lacus ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  wenia veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commos commodo consequat.</p>
-
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur <a href="">embeded link</a> embeded link hover non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium dolore. mque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
-			
-			<blockquote>Pull quote style dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</blockquote>
-			
-			<h2>Lorem ipsum dolor sit amet esse dolore pariatur dolore esse sed ut derp voliptate velit esee</h2>
-			
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sinmmodo viverra maecenas estosee accumsan lacus ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			
-			<ul>
-				<li> Risus commodo viverra maecenas accumsan lacus.</li>
-				<li>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</li>
-				<li>Risus commodo viverra maecenas accumsan lacus.</li>
-			</ul>
-			
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sinmmodo viverra maecenas estosee accumsan lacus ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-			
-			<ol>
-				<li>Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</li>
-				<li>TRisus commodo viverra maecenas accumsan lacus.</li>
-			</ol>
-			
-			<p>Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  wenia veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commos commodo consequat.</p>
-			
-			<h3>H3 Lorem ipsum dolor sit amet consectetur</h3>
-			
-			<p>Duis aute irure Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim  wenia veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commos commodo consequat.</p>
-			
+			<?php get_template_part( 'loop', 'page' ); ?>
+							
 		</div><!-- inner_content -->
 		
 		<?php get_sidebar(); ?>
@@ -50,10 +19,5 @@
 	</div><!-- container -->
 
 </div><!-- internal_main -->
-
-	<?php // get_template_part( 'loop', 'page' ); ?>
-		
-	
-
 
 <?php get_footer(); ?>
