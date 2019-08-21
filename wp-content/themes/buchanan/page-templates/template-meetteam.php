f2b928cd69084bfa7ec7cb493048421c5e16bc31
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-21 11:45:51 -0700

    push

--- wp-content/themes/buchanan/page-templates/template-meetteam.php
+++ wp-content/themes/buchanan/page-templates/template-meetteam.php
@@ -4,7 +4,151 @@
 
 get_header(); ?>
 
+<div id="internal_main">
+	
+	<img class="internal_circles" src="<?php bloginfo('template_directory');?>/images/graphic-dots.svg"/>
+	
+	<div class="internal_wrapper">
+		
+		<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
+		
+		<div class="meet_team_wrapper">
+			
+			<div class="single_att">
+				
+				<a class="" href="">
+				
+					<div class="att_img_wrapper">
+						
+						<img class="att_img" src="<?php bloginfo('template_directory');?>/images/profile-jbuchanan.jpg"/>
+					
+						<div class="att_overlay">
+						
+							<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+						
+							<span>View Profile</span>
+						
+						</div><!-- att_overlay -->
+					
+					</div><!-- att_img_wrapper -->
+					
+					<span class="att_name">Robert J. Buchanan</span>
+					
+					<span class="att_position">Attorney and Managing Partner</span><!-- att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a class="" href="">
+				
+					<div class="att_img_wrapper">
+						
+						<img class="att_img" src="<?php bloginfo('template_directory');?>/images/profile-jbuchanan.jpg"/>
+					
+						<div class="att_overlay">
+						
+							<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+						
+							<span>View Profile</span>
+						
+						</div><!-- att_overlay -->
+					
+					</div><!-- att_img_wrapper -->
+					
+					<span class="att_name">Robert J. Buchanan</span>
+					
+					<span class="att_position">Attorney and Managing Partner</span><!-- att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a class="" href="">
+				
+					<div class="att_img_wrapper">
+						
+						<img class="att_img" src="<?php bloginfo('template_directory');?>/images/profile-jbuchanan.jpg"/>
+					
+						<div class="att_overlay">
+						
+							<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+						
+							<span>View Profile</span>
+						
+						</div><!-- att_overlay -->
+					
+					</div><!-- att_img_wrapper -->
+					
+					<span class="att_name">Robert J. Buchanan</span>
+					
+					<span class="att_position">Attorney and Managing Partner</span><!-- att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a class="" href="">
+				
+					<div class="att_img_wrapper">
+						
+						<img class="att_img" src="<?php bloginfo('template_directory');?>/images/profile-jbuchanan.jpg"/>
+					
+						<div class="att_overlay">
+						
+							<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+						
+							<span>View Profile</span>
+						
+						</div><!-- att_overlay -->
+					
+					</div><!-- att_img_wrapper -->
+					
+					<span class="att_name">Robert J. Buchanan</span>
+					
+					<span class="att_position">Attorney and Managing Partner</span><!-- att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+			<div class="single_att">
+				
+				<a class="" href="">
+				
+					<div class="att_img_wrapper">
+						
+						<img class="att_img" src="<?php bloginfo('template_directory');?>/images/profile-jbuchanan.jpg"/>
+					
+						<div class="att_overlay">
+						
+							<?php echo file_get_contents("wp-content/themes/buchanan/images/arrow.svg"); ?>
+						
+							<span>View Profile</span>
+						
+						</div><!-- att_overlay -->
+					
+					</div><!-- att_img_wrapper -->
+					
+					<span class="att_name">Robert J. Buchanan</span>
+					
+					<span class="att_position">Attorney and Managing Partner</span><!-- att_position -->
+				
+				</a>
+				
+			</div><!-- single_att -->
+			
+		</div><!-- meet_team_wrapper -->
+				
+	</div><!-- internal_wrapper -->
 
+</div><!-- internal_main -->	
 
 
 <?php get_footer(); ?>
