6cffa962db5d68b51f86297ef8633ff2ee0728a8
Garrett Cullen <Garth619@users.noreply.github.com>
2019-08-21 16:12:38 -0700

    push

--- wp-content/themes/buchanan/page-templates/template-about.php
+++ wp-content/themes/buchanan/page-templates/template-about.php
@@ -14,13 +14,13 @@ get_header(); ?>
 		
 			<h1 class="internal_header"><?php the_title();?></h1><!-- internal_header -->
 		
-			<span class="about_firm_subtitle">INjURY ATTORNEYS RIGHTING WRONGS</span><!-- about_firm_subtitle -->
+			<span class="about_firm_subtitle"><?php the_field( 'about_subtitle' ); ?></span><!-- about_firm_subtitle -->
 		
 			<div class="about_firm_intro">
 			
-			<p>Our team represents real people injured by vehicle accidents or medical negligence.  When you contact our firm, we give you a free evaluation and answer your questions.</p>
+				<?php the_field( 'about_intro' ); ?>
 			
-		</div><!-- about_firm_intro -->
+			</div><!-- about_firm_intro -->
 		
 		</div><!-- about_intro_wrapper -->
 		
@@ -28,11 +28,7 @@ get_header(); ?>
 			
 			<div class="about_top_left content">
 				
-				<p>Our clients always meet with the team that handles the claim. We never pass you down to an inexperienced attorney or send you off to another firm to make a commission, like television or billboard lawyers. And we work tirelessly on your case to win, and do so at no additional cost to you. Using cutting-edge technology and legal video documentaries, we show the wrongdoers and their insurance companies they must settle the claim or risk losing at trial. We are in it to win it for you.</p>
-
-				<h2>Our Focus</h2>
-				
-				<p>Our team is focused on righting wrongs. We seek fair and just reimbursement for the wrongly injured. We’ve been recognized nationally for successfully helping catastrophically injured people and families of those who have died because of misconduct of others. We handle only severe accident and negligence claims. The people we choose to represent are honorable; they are clear exceptions to the tales and distortions in the media of lawsuit abuses.</p>
+				<?php the_field( 'about_top_content' ); ?>
 				
 			</div><!-- about_top_left -->
 			
@@ -42,9 +38,15 @@ get_header(); ?>
 					
 					<div class="about_wistia"></div><!-- about_wistia -->
 					
-					<img class="about_video_thumb" src="<?php bloginfo('template_directory');?>/images/int-about-img.jpg"/><!-- about_video_thumb -->
+					<?php $video_thumb = get_field( 'video_thumb' ); ?>
+
+					<img class="about_video_thumb" src="<?php echo $video_thumb['url']; ?>" alt="<?php echo $video_thumb['alt']; ?>" />
+
+					<div class="about_play_wrapper">
 					
-					<img class="about_play" src="<?php bloginfo('template_directory');?>/images/hero-vid-playbutton.png"/>
+						<img class="about_play" src="<?php bloginfo('template_directory');?>/images/hero-vid-playbutton.png"/>
+					
+					</div><!-- about_play_wrapper -->
 					
 				</div><!-- about_top_video -->
 				
@@ -58,7 +60,7 @@ get_header(); ?>
 			
 			<div class="about_middle_quote">
 				
-				<p> We seek fair and just reimbursement for the wrongly injured. We’ve been recognized nationally for successfully helping catastrophically injured people and families of those who have died because of misconduct of others.</p>
+				<?php the_field( 'about_middle_content' ); ?>
 				
 				<a class="button" href="#consultation">Schedule a Free Consultation</a>
 				
@@ -70,33 +72,13 @@ get_header(); ?>
 			
 			<div class="about_bottom_col">
 				
-				<h3>pASSIONATE DEDICATION TO OUR CLIENTS</h3>
-				
-				<p>Unlike television and billboard lawyers and other law firms, we treat our clients like family. In fact, our on-staff nurse Helen Hicks MSN is a former client of ours. She liked us so much she wanted to work here instead of for a hospital.</p>
-
-				<p>We work closely with our clients, from initial interview through final settlement or trial. We speak the right language, we select only the best claims, and know what we are doing. You’ve come to our firm’s website because you have important questions and are looking to hire the right team.  If you’ve suffered a catastrophic injury or lost a loved one because of the misconduct of others, there is no question – we are the right team for you.</p>
-
-
-				<h3>CUTTING-EDGE TECHNOLOGY</h3>
-				
-				<p>We’ve learned how to captivate and hold the attention of juries and answer their questions and concerns using high-definition video, computer animation, digital media, and the latest technology. We explain the medical issues in plain language and demonstrate the real impact of our clients’ resulting harms, losses, and disabling injuries.  And, we talk to jurors as caring and intelligent human beings, making decisions that will forever affect our clients’ lives.</p>
-
-				<p>We’ve learned not only to build powerful claims and to always arrive at trial exceedingly well prepared, but also to present our clients’ cases in the clear, compelling manner they deserve. Because ultimately what matters isn’t what an attorney, doctor, or expert says. It’s what jurors comprehend – and believe.</p>
+				<?php the_field( 'about_bottom_column_one' ); ?>
 				
 			</div><!-- about_bottom_col -->
 			
 			<div class="about_bottom_col">
 				
-				<h3>ON-STAFF MEDICAL EXPERTS</h3>
-				
-				<p>Serious injury and death cases require exceptional attorney skill, knowledge, and experience. But that’s just half the equation. Cases involving catastrophic injuries or death from an accident or medical negligence (such as brain injury, birth trauma, paralysis, disfigurement, severe burns, or wrongful death) require an experienced medical team like ours to navigate each case’s inherent complexities.</p>
-
-				<p>At Buchanan & Buchanan we employ both qualified and specialized medical professionals who work closely with our experienced legal staff to protect and enforce your legal rights.</p>
-
-
-				<h3>TOP RATED AND RESPECTED IN THE LEGAL COMMUNITY</h3>
-				
-				<p>One of the most important decisions a person can make after suffering a traumatic injury or loss in the family is selecting the right law firm team to help and guide them. The Buchanan team has compassionate people who know what you are going through and an exceptional track record for success in personal injury lawsuits.  The firm’s managing partner, Robert Buchanan, is an officer of the State Bar of Michigan, the Supreme Court’s governing body over all attorneys in Michigan. The State Bar of Michigan protects the public and promotes justice and fairness for everyone. Rob is also a professor of medical malpractice law for Western Michigan University College of Law.</p>
+				<?php the_field( 'about_bottom_column_two' ); ?>
 				
 			</div><!-- about_bottom_col -->
 			
